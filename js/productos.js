82a59bf4c361f640186e70253025b2fd14bdaefc
elcaracol32 <balti2000@hotmail.com>
2023-08-14 19:02:41 -0300

    tercera preentrega JS

---
+++ js/productos.js
@@ -0,0 +1,35 @@
+const productos = [
+
+    {id:1,
+    nombre:"sprite",
+    precio: 500,
+    cantidad: 1,
+    img:"https://labebidadetusfiestas.com.ar/46004/sprite-limonada-descartable-1500cc.jpg"},
+
+
+    {id:2,
+    nombre:"cocacola",
+    precio: 600,
+    cantidad: 1,
+    img:"https://quirinobebidas.com.ar/wp-content/uploads/2022/10/productos-2022-10-04T111414.437.png"},
+
+    {id:3,
+    nombre:"levite",
+    precio: 700,
+    cantidad: 1,
+    img:"https://static.wixstatic.com/media/d2b1c5_aef1d994a21b45f790bdbf4706e3d343~mv2.jpg/v1/fill/w_480,h_480,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d2b1c5_aef1d994a21b45f790bdbf4706e3d343~mv2.jpg"},
+
+
+    {id:4,
+    nombre:"fernet",
+    precio: 800,
+    cantidad: 1,
+    img:"https://carrefourar.vtexassets.com/arquivos/ids/226775/7790290101602_02.jpg?v=637715449494370000"},
+
+
+    {id:5,
+    nombre:"whisky",
+    precio: 900,
+    cantidad: 1,
+    img:"https://vinotecalavia.com/5036-thickbox_default/w-johnnie-walker-blue-label-.jpg"},
+]
